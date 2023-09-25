01d0e26672167da075e1e3001851ad0365f5fa13
Leela57 <141907651+Leela57@users.noreply.github.com>
2023-09-25 17:00:38 +0530

    Add files via upload

--- Appium/Project/googleTasks.java
+++ Appium/Project/googleTasks.java
@@ -1,17 +1,27 @@
 package project_appium;
-
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
-import org.testng.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-public class googleTasks {
+import static org.testng.Assert.assertEquals;
+
+public class googleTasks{
     AndroidDriver driver;
+    WebDriverWait wait;
     @BeforeClass
     public void setUp() throws MalformedURLException {
         // Desired Capabilities
@@ -21,24 +31,36 @@ public class googleTasks {
         options.setAppPackage("com.google.android.apps.tasks");
         options.setAppActivity(".ui.TaskListsActivity");
         options.noReset();
+        // Server Address
         URL serverURL = new URL("http://localhost:4723/wd/hub");
+        // Driver Initialization
         driver = new AndroidDriver(serverURL, options);
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+        wait=new WebDriverWait(driver,Duration.ofSeconds(3));
     }
     @Test
-    public void googleTaskTest() throws InterruptedException {
-        driver.findElement(AppiumBy.accessibilityId("Create new task")).click();
-        driver.findElement(AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.ScrollView/android.widget.LinearLayout/android.widget.EditText")).sendKeys("Complete Activity with Google Tasks");
-        driver.findElement(AppiumBy.id("com.google.android.apps.tasks:id/add_task_done")).click();
-        Thread.sleep(1000);
-        driver.findElement(AppiumBy.accessibilityId("Create new task")).click();
-        Thread.sleep(1000);
-        driver.findElement(AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.ScrollView/android.widget.LinearLayout/android.widget.EditText")).sendKeys("Complete Activity with Google Keep");
-        driver.findElement(AppiumBy.id("com.google.android.apps.tasks:id/add_task_done")).click();
-        Thread.sleep(1000);
-        driver.findElement(AppiumBy.accessibilityId("Create new task")).click();
-        Thread.sleep(1000);
-        driver.findElement(AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.ScrollView/android.widget.LinearLayout/android.widget.EditText")).sendKeys("Complete the second Activity Google Keep");
-        driver.findElement(AppiumBy.id("com.google.android.apps.tasks:id/add_task_done")).click();
+    public void addTasks() throws InterruptedException {
+        List<String> taskList= new ArrayList<>();
+        taskList.add("Complete Activity with Google Tasks");
+        taskList.add("Complete Activity with Google Keep");
+        taskList.add("Complete the second Activity Google Keep");
 
+        for (String task:taskList) {
+            driver.findElement(AppiumBy.accessibilityId("Create new task")).click();
+            Reporter.log("--Clicked on add new task--",true);
+
+            driver.findElement(AppiumBy.id("com.google.android.apps.tasks:id/add_task_title")).sendKeys(task);
+            driver.findElement(AppiumBy.id("com.google.android.apps.tasks:id/add_task_done")).click();
+            Reporter.log("--Added task:"+task+"--",true);
+        }
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(AppiumBy.accessibilityId("Mark as complete"))));
+        List<WebElement> addedLists=driver.findElements(AppiumBy.id("com.google.android.apps.tasks:id/task_name"));
+        assertEquals(addedLists.size(),3);
+    }
+    @AfterClass
+    public void tearDown() {
+        // Close the app
+        driver.quit();
     }
 }
+
